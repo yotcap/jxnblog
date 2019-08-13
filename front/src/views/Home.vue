93b0d78e948d774b9339a54b61fd9ead9436a7c2
jxn2802 <jxn2802@gmail.com>
2019-08-13 22:07:48 +0800

    explore ways to use ts

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -12,22 +12,32 @@ import { Component, Vue } from 'vue-property-decorator';
     return {
       page: 1,
       searchCondition: '',
-    }
+    };
   },
-  beforeMount() {
-    let { condition, page } = this.$route.query;
-    this.getData({condition, page});
-  },
-  methods: {
-    getData({condition, page}) {
-      console.log(condition, page);
-    },
-  },
+  // beforeMount() {
+  //   const { condition, page } = this.$route.query;
+  //   (this as any).getData({condition, page});
+  // },
+  // methods: {
+  //   getData({condition, page}) {
+  //     console.log(condition, page);
+  //   },
+  // },
   // components: {
   //   HelloWorld,
   // },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private page: number = 1;
+  private searchCondition: string = '';
+  private beforeMount() {
+    // const { condition, page } = this.$route.query;
+    (this as any).getData(this.$route.query);
+  }
+  private getData({condition, page}) {
+    console.log(condition, page);
+  }
+}
 </script>
 <style lang="less" scoped>
 .home {
