72c65ed5c27f3ed3cf3283b72b71b653a40bfdcc
yotcap <yotcap@163.com>
2019-10-20 21:20:09 +0800

    Temp add env about initData

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+global.DB_URL_BUILD = 'mongodb://jxn:jxncap@1.0@127.0.0.1:27017/jxndb';
 const DB_URL = global.DB_URL_BUILD || 'mongodb://127.0.0.1:27018/jxblog';
 
 mongoose.connection.openUri(DB_URL);
