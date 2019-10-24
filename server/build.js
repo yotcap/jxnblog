002d7e661232f038645725c1e6c9b9e8befa7f1e
yotcap <yotcap@163.com>
2019-10-24 21:42:27 +0800

    Add server config file

--- server/build.js
+++ server/build.js
@@ -1,3 +1,9 @@
-global.DB_URL_BUILD = 'mongodb://jxn:jxncap1.0@127.0.0.1:27017/jxndb';
+require('./config');
+
+const user = global.USER?global.USER:'xiaoming';
+const pwd = global.PWD?global.PWD:'xm123';
+const db = global.DB?global.DB:'xiaomingdb';
+
+global.DB_URL_BUILD = `mongodb://${user}:${pwd}@127.0.0.1:27017/${db}`;
 
 require('./index');
