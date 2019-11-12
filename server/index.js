cb3a37386a88ca1df845b2d9a687f0de685f8251
yotcap <yotcap@163.com>
2019-11-12 13:10:21 +0800

    FIx CROS bugs in the upload API

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const errorLogStream = rfs('error.log', {
 });
 
 app.all('*', (req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', 'http://yotcap.top:8086');
   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
   // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   // res.header("X-Powered-By",' 3.2.1');
