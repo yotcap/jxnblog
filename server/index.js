286bbc70b4af308e387d45737ad68089fa135de5
yotcap <jxni@outlook.com>
2021-07-11 19:21:17 +0800

    尝试修复appstore api

--- server/index.js
+++ server/index.js
@@ -64,6 +64,7 @@ app.use(BASE_PATH+'/static', (req, res, next) => {
         res.setHeader('Connection', 'keep-alive');
         res.setHeader('Content-Type', 'text/plain; charset=utf-8');
         res.setHeader('Cache-Control', 'max-age=300');
+        res.setHeader('Accept-Ranges', 'bytes');
         // res.setHeader('Content-Encoding', 'gzip');
         next();
       } else {
